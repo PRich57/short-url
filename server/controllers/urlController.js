aaa01ec0767ad35e8081c817715ee620042e4ab9
PRich57 <pcrichards57@gmail.com>
2023-12-13 17:49:18 -0700

    Start working on the urlController

---
+++ server/controllers/urlController.js
@@ -0,0 +1,11 @@
+const Url = require('../models');
+const shortid = require('short-unique-id');
+
+exports.shortenUrl = async (req, res) => {
+  const { originalUrl, user, customSlug } = req.body;
+  try {
+    // Check if the customSlug
+  } catch (err) {
+    
+  }
+}
