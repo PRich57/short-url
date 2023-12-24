3aac165e34ac6a36b195dbe9867a1f337d06fada
PRich57 <pcrichards57@gmail.com>
2023-12-24 12:22:51 -0500

    Update client auth with custom url

--- client/src/utils/auth/auth.js
+++ client/src/utils/auth/auth.js
@@ -3,7 +3,7 @@ import { setContext } from "@apollo/client/link/context";
 
 // Use deployed while in production and localhost in development
 const httpLink = createHttpLink({
-  uri: process.env.NODE_ENV === 'production' ? 'https://short-url50-ca670a86f511.herokuapp.com/graphql' : 'http://localhost:3001/graphql',
+  uri: process.env.NODE_ENV === 'production' ? 'https://www.you-rl.xyz/graphql' : 'http://localhost:3001/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
