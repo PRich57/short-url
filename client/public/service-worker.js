8e35b0b723ff85f8b3d2780a80d8bccf73caa4d8
PRich57 <pcrichards57@gmail.com>
2024-01-24 10:59:54 -0700

    Modify manifest and add service-worker for PWA

---
+++ client/public/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'version-1';
+const urlsToCache = [ 'index.html', 'offline.html' ];
+
+// 'install' event listener
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  )
+});
+
+// 'fetch' event listener
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(() => {
+        return fetch(event.request)
+          .catch(() => caches.match('offline.html'));
+      })
+  );
+});
+
+// 'activate' event listener
+self.addEventListener('activate', (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => Promise.all(
+      cacheNames.map((cacheName) => {
+        if (!cacheWhiteList.includes(cacheName)) {
+          return caches.delete(cacheName);
+        }
+      })
+    ))
+  );
+});
