8545179164a15a8cc20f24168a94f310e086209a
PRich57 <pcrichards57@gmail.com>
2024-01-26 09:59:24 -0700

    Add white space to test rebuild

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -2,6 +2,7 @@ const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+
 const expiration = '24h';
 const JWT_SECRET = process.env.JWT_SECRET;
 
