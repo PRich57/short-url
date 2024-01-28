f7272341e5d4f05843815f76adcf0f7d307e63f0
PRich57 <pcrichards57@gmail.com>
2024-01-28 12:50:33 -0700

    Modify index.html and vite.config for VitePWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'favicon-32x32.png', 'favicon-16x16.png'],
+      manifest: {
+        name: 'YouRL',
+        short_name: 'YouRL',
+        description: 'URL Shortening Application',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ],
   server: {
     port: 5179,
     open: true,
