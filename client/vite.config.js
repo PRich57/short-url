e0727bfb97357a2b253156db1d70f59e83f98b81
PRich57 <pcrichards57@gmail.com>
2024-01-29 09:23:31 -0700

    Comment out VitePWA to retest build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
