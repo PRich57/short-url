c7be288d46147a4463b93499b494fb1899e9fa16
PRich57 <pcrichards57@gmail.com>
2024-01-29 09:19:38 -0700

    Comment out VitePWA to retest build

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,43 +7,43 @@ export default defineConfig({
     react(),
     VitePWA({
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'favicon-32x32.png', 'favicon-16x16.png'],
-      manifest: {
-        name: 'YouRL',
-        short_name: 'YouRL',
-        description: 'URL Shortening Application',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          }
-        ]
-      },
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      workbox: {
-      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      }
-    })
-  ],
+  //     manifest: {
+  //       name: 'YouRL',
+  //       short_name: 'YouRL',
+  //       description: 'URL Shortening Application',
+  //       theme_color: '#ffffff',
+  //       icons: [
+  //         {
+  //           src: '/android-chrome-192x192.png',
+  //           sizes: '192x192',
+  //           type: 'image/png'
+  //         },
+  //         {
+  //           src: '/android-chrome-512x512.png',
+  //           sizes: '512x512',
+  //           type: 'image/png'
+  //         },
+  //         {
+  //           src: '/android-chrome-512x512.png',
+  //           sizes: '512x512',
+  //           type: 'image/png',
+  //           purpose: 'any'
+  //         },
+  //         {
+  //           src: '/android-chrome-512x512.png',
+  //           sizes: '512x512',
+  //           type: 'image/png',
+  //           purpose: 'maskable'
+  //         }
+  //       ]
+  //     },
+  //     registerType: 'autoUpdate',
+  //     injectRegister: 'auto',
+  //     workbox: {
+  //     globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+  //     }
+  //   })
+  // ],
   server: {
     port: 5179,
     open: true,
